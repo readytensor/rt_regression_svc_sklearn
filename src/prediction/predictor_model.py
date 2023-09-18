d35168b3e2505dbd467bdca1eb06a66ef41e2ee7
Abu Desai <abhyuday.desai@gmail.com>
2023-09-17 18:25:58 -0700

    first commit

---
+++ src/prediction/predictor_model.py
@@ -0,0 +1,202 @@
+import os
+import warnings
+from typing import Optional
+
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.svm import SVR
+from sklearn.exceptions import NotFittedError
+
+warnings.filterwarnings("ignore")
+
+
+PREDICTOR_FILE_NAME = "predictor.joblib"
+
+
+class Regressor:
+    """A wrapper class for the SVR Regressor.
+
+    This class provides a consistent interface that can be used with other
+    regressor models.
+    """
+
+    model_name = "SVR Regressor"
+
+    def __init__(
+        self,
+        C: Optional[float] = 1.0,
+        kernel: Optional[str] = "rbf",
+        degree: Optional[int] = 3,
+        **kwargs,
+    ):
+        """Construct a new SVR Regressor.
+
+        Args:
+            C (float, optional): Inverse of regularization strength; must be a positive
+                float. Like in support vector machines, smaller values specify
+                stronger regularization. Must be strictly positive (C >= 0.)
+                Defaults to 1.0.
+            kernel (int, optional): Specifies the kernel type to be used
+                in the algorithm.
+                Options to consider: ["linear", "poly", "rbf"]
+                Defaults to "rbf".
+            degree (int, optional): Degree of the polynomial kernel
+                function (poly). This argument is ignored for all other kernel
+                functions. Must be >= 1.
+                Defaults to 3.
+        """
+        self.C = float(C)
+        self.kernel = kernel
+        self.degree = int(degree)
+        self.model = self.build_model()
+        self._is_trained = False
+
+    def build_model(self) -> SVR:
+        """Build a new regressor."""
+        model = SVR(
+            C=self.C,
+            kernel=self.kernel,
+            degree=self.degree
+        )
+        return model
+
+    def fit(self, train_inputs: pd.DataFrame, train_targets: pd.Series) -> None:
+        """Fit the regressor to the training data.
+
+        Args:
+            train_inputs (pandas.DataFrame): The features of the training data.
+            train_targets (pandas.Series): The labels of the training data.
+        """
+        self.model.fit(train_inputs, train_targets)
+        self._is_trained = True
+
+    def predict(self, inputs: pd.DataFrame) -> np.ndarray:
+        """Predict regression targets for the given data.
+
+        Args:
+            inputs (pandas.DataFrame): The input data.
+        Returns:
+            numpy.ndarray: The predicted regression targets.
+        """
+        return self.model.predict(inputs)
+
+    def evaluate(self, test_inputs: pd.DataFrame, test_targets: pd.Series) -> float:
+        """Evaluate the regressor and return the r-squared score.
+
+        Args:
+            test_inputs (pandas.DataFrame): The features of the test data.
+            test_targets (pandas.Series): The targets of the test data.
+        Returns:
+            float: The r-squared score of the regressor.
+        """
+        if self.model is not None:
+            return self.model.score(test_inputs, test_targets)
+        raise NotFittedError("Model is not fitted yet.")
+
+    def save(self, model_dir_path: str) -> None:
+        """Save the regressor to disk.
+
+        Args:
+            model_dir_path (str): Dir path to which to save the model.
+        """
+        if not self._is_trained:
+            raise NotFittedError("Model is not fitted yet.")
+        joblib.dump(self, os.path.join(model_dir_path, PREDICTOR_FILE_NAME))
+
+    @classmethod
+    def load(cls, model_dir_path: str) -> "Regressor":
+        """Load the regressor from disk.
+
+        Args:
+            model_dir_path (str): Dir path to the saved model.
+        Returns:
+            Regressor: A new instance of the loaded regressor.
+        """
+        model = joblib.load(os.path.join(model_dir_path, PREDICTOR_FILE_NAME))
+        return model
+
+    def __str__(self):
+        # sort params alphabetically for unit test to run successfully
+        return (
+            f"Model name: {self.model_name} ("
+            f"C: {self.C}, "
+            f"degree: {self.degree}, "
+            f"kernel: {self.kernel})"
+        )
+
+
+def train_predictor_model(
+    train_inputs: pd.DataFrame, train_targets: pd.Series, hyperparameters: dict
+) -> Regressor:
+    """
+    Instantiate and train the predictor model.
+
+    Args:
+        train_X (pd.DataFrame): The training data inputs.
+        train_y (pd.Series): The training data targets.
+        hyperparameters (dict): Hyperparameters for the regressor.
+
+    Returns:
+        'Regressor': The regressor model
+    """
+    regressor = Regressor(**hyperparameters)
+    regressor.fit(train_inputs=train_inputs, train_targets=train_targets)
+    return regressor
+
+
+def predict_with_model(regressor: Regressor, data: pd.DataFrame) -> np.ndarray:
+    """
+    Predict regression targets for the given data.
+
+    Args:
+        regressor (Regressor): The regressor model.
+        data (pd.DataFrame): The input data.
+
+    Returns:
+        np.ndarray: The predicted regression targets.
+    """
+    return regressor.predict(data)
+
+
+def save_predictor_model(model: Regressor, predictor_dir_path: str) -> None:
+    """
+    Save the regressor model to disk.
+
+    Args:
+        model (Regressor): The regressor model to save.
+        predictor_dir_path (str): Dir path to which to save the model.
+    """
+    if not os.path.exists(predictor_dir_path):
+        os.makedirs(predictor_dir_path)
+    model.save(predictor_dir_path)
+
+
+def load_predictor_model(predictor_dir_path: str) -> Regressor:
+    """
+    Load the regressor model from disk.
+
+    Args:
+        predictor_dir_path (str): Dir path where model is saved.
+
+    Returns:
+        Regressor: A new instance of the loaded regressor model.
+    """
+    return Regressor.load(predictor_dir_path)
+
+
+def evaluate_predictor_model(
+    model: Regressor, x_test: pd.DataFrame, y_test: pd.Series
+) -> float:
+    """
+    Evaluate the regressor model and return the r-squared value.
+
+    Args:
+        model (Regressor): The regressor model.
+        x_test (pd.DataFrame): The features of the test data.
+        y_test (pd.Series): The targets of the test data.
+
+    Returns:
+        float: The r-sq value of the regressor model.
+    """
+    return model.evaluate(x_test, y_test)
